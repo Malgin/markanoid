da1e1977a9a8759e3fa766f9a2e94ecdd9d9cea1
Vitaliy Lebedev <malgin05@gmail.com>
2017-09-28 23:14:26 +0300

    Initial Commit

---
+++ src/Application.js
@@ -0,0 +1,25 @@
+import ui.TextView as TextView;
+
+exports = Class(GC.Application, function () {
+
+  this.initUI = function () {
+
+    // initiate backscreen 
+
+    this.tvHelloWorld = new TextView({
+      superview: this.view,
+      text: 'Hello, world!',
+      color: 'white',
+      x: 0,
+      y: 100,
+      width: this.view.style.width,
+      height: 100
+    });
+
+  };
+
+  this.launchUI = function () {
+
+  };
+
+});
