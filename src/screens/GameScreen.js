68b8f2183f4fbf1dc2d5e096eb399805d4960f53
Vitaliy Lebedev <malgin05@gmail.com>
2017-09-29 22:56:46 +0300

    Make ball bounce off paddle

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -7,7 +7,7 @@ import ui.ImageView as ImageView;
 import ..models.Paddle as Paddle;
 import ..models.Ball as Ball;
 
-const BOUNCABLE_BORDER_WIDTH = Ball.BALL_RADIUS * 2 + 10;
+const BOUNCABLE_BORDER_WIDTH = 10;
 
 exports = Class(ImageView, function(supr) {
 
@@ -46,15 +46,11 @@ exports = Class(ImageView, function(supr) {
     this.on('InputMove', bind(this, function (event, point) {
 
       if (point.x > (Paddle.PADDLE_WIDTH / 2) && point.x < (this.width - Paddle.PADDLE_WIDTH / 2)) {
-        this._playerPaddle.updateOpts({
-          x: point.x - (Paddle.PADDLE_WIDTH / 2)
-        });
+        this._playerPaddle.style.x = point.x - (Paddle.PADDLE_WIDTH / 2);
       }
 
       if (!this._ball.moving) {
-        this._ball.updateOpts({
-          x: this._playerPaddle.style.x + (Paddle.PADDLE_WIDTH / 2) - Ball.BALL_RADIUS
-        });
+        this._ball.style.x = this._playerPaddle.style.x + (Paddle.PADDLE_WIDTH / 2) - Ball.BALL_RADIUS;
       }
     }));
 
@@ -67,17 +63,38 @@ exports = Class(ImageView, function(supr) {
   };
 
   this.tick = function (dt) {
+
     // make ball bounce off field edges
     if (this._ball.moving) {
-      if (this._ball.style.x >= this.width - BOUNCABLE_BORDER_WIDTH ||
-          this._ball.style.x <= BOUNCABLE_BORDER_WIDTH) {
+      if (this._ball.style.x >= this.width - (Ball.BALL_RADIUS * 2 + BOUNCABLE_BORDER_WIDTH) || this._ball.style.x <= BOUNCABLE_BORDER_WIDTH) {
         this._ball.velocity.x = -1 * this._ball.velocity.x; // bounce off walls
       }
 
-      if (this._ball.style.y <= BOUNCABLE_BORDER_WIDTH) {
-        this._ball.velocity.y = -1 * this._ball.velocity.y; // bounce off ceiling
+      if (this._ball.style.y <= BOUNCABLE_BORDER_WIDTH ||
+          this._ballCollidesWithPaddle() ||
+          this._ball.style.y >= this.height - BOUNCABLE_BORDER_WIDTH) {
+        this._ball.velocity.y = -1 * this._ball.velocity.y; // bounce off ceiling or paddle
       }
+
+      // TODO make ball appear on other side of an edge
     }
-    // TODO make ball appear on other side of an edge
+
+  };
+
+  this._ballCollidesWithPaddle = function () {
+    var ballPosition = {
+      x: this._ball.style.x,
+      y: this._ball.style.y
+    };
+
+    var paddlePosition = {
+      x: this._playerPaddle.style.x,
+      y: this._playerPaddle.style.y
+    };
+
+    var intersectsPaddleVertically = ballPosition.y + Ball.BALL_RADIUS * 2 > paddlePosition.y && ballPosition.y + Ball.BALL_RADIUS * 2 <= paddlePosition.y + 4; // TODO fix magic number
+    var intersectsPaddleHorizontally = ballPosition.x >= (paddlePosition.x - Ball.BALL_RADIUS) && ballPosition.x <= (paddlePosition.x + Paddle.PADDLE_WIDTH - Ball.BALL_RADIUS);
+
+    return this._ball.moving && intersectsPaddleVertically && intersectsPaddleHorizontally;
   }
 });
