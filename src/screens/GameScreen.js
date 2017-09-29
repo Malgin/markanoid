f435640a9267ac8f9cbbb5e39c8a402fd18378b0
Vitaliy Lebedev <malgin05@gmail.com>
2017-09-29 20:31:42 +0300

    Add a ball.

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -6,12 +6,11 @@ const PADDLE_WIDTH = 100;
 const PADDLE_HEIGHT = 15;
 const PADDLE_BOTTOM_PADDING = 50;
 
+const BALL_RADIUS = 15;
+
 exports = Class(ImageView, function(supr) {
 
   this.init = function(opts) {
-
-    this.width = device.width / device.devicePixelRatio;
-    this.height = device.height / device.devicePixelRatio;
 
     opts = merge(opts, {
       image: 'resources/images/space_pixel_background.png'
@@ -24,10 +23,16 @@ exports = Class(ImageView, function(supr) {
 
   this.build = function() {
 
+    this.width = device.width / device.devicePixelRatio;
+    this.height = device.height / device.devicePixelRatio;
+
+    this.initialPaddleX = (this.width / 2) - (PADDLE_WIDTH / 2);
+    this.initialPaddleY = (this.height) - PADDLE_BOTTOM_PADDING - (PADDLE_HEIGHT / 2);
+
     this._playerPaddle = new ImageView({
       superview: this,
-      x: (this.width / 2) - (PADDLE_WIDTH / 2),
-      y: (this.height) - PADDLE_BOTTOM_PADDING - (PADDLE_HEIGHT / 2),
+      x: this.initialPaddleX,
+      y: this.initialPaddleY,
       width: PADDLE_WIDTH,
       height: PADDLE_HEIGHT,
       canHandleEvents: false,
@@ -42,6 +47,18 @@ exports = Class(ImageView, function(supr) {
         });
       }
     }));
+
+    // spawn a ball
+    this._ball = new ImageView({
+      superview: this,
+      image: 'resources/images/fireball.png',
+      x: this.initialPaddleX + (PADDLE_WIDTH / 2) - BALL_RADIUS,
+      y: this.initialPaddleY - (BALL_RADIUS * 2),
+      width: BALL_RADIUS * 2,
+      height: BALL_RADIUS * 2
+    });
+
+    console.log('test');
 
     // TODO build blocks layers
   };
